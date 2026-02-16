a1e4927be62ed27ccb46d2a24fbafb2a7b6bb10c
HowardCui <HowardCui@users.noreply.github.com>
2026-02-16 06:10:58 -0500

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 # time: 2026/02/13
 # name: Haowen Cui, Yuhan Guo
+
+if __name__ == "__main__":
+    # dataset check
+    df=dataset.copy()
+    print(df.shape)
+    print(df.columns)
+    print(df.dtypes)
+    print(df.isna().mean().sort_values(ascending=False).head(20))
