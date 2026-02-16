da94d6537981708aa5df8ccd291f4e064fe0ae68
HowardCui <HowardCui@users.noreply.github.com>
2026-02-16 07:52:06 -0500

    Update dataset_preview.py

--- dataset_preview.py
+++ dataset_preview.py
@@ -16,18 +16,23 @@ def init_data_check(dataset):
     """
     df = dataset.copy()
     # dataset dimensions
+    print('dataset dimensions')
     print(df.shape)
     print('-')
     # Check column names
+    print('column names')
     print(df.columns)
     print('-')
     # Review data types
+    print('data types')
     print(df.dtypes)
     print('-')
     # Calculate missing value proportions
+    print('missing value proportions:')
     print(df.isna().mean().sort_values(ascending=False).head(20))
     print('-')
     # Check duplicate rows
+    print('duplicate rows')
     print(df.duplicated().sum())
     # Check temporal consistency of records.
     print((df["yearstart"] != df["yearend"]).sum())
