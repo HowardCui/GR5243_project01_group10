7468aeaf400a884c20f448fe1dc85353c674f0ee
Yuhan Guo <yg2695@columbia.edu>
2026-02-18 22:59:58 -0500

    add header for EDA.py

--- EDA.py
+++ EDA.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python 3.12
+# -*- coding: utf-8 -*-
+# time: 2026/02/13
+# name: Haowen Cui, Yuhan Guo
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from acquire import get_dataset
