cdb2ba4859926247a250fd83f06ced0f7736a7a1
Syer_r <113523891+Skyerrrrrrr@users.noreply.github.com>
2026-02-20 01:28:30 -0500

    EDA.py

--- EDA.py
+++ EDA.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 3.12
 # -*- coding: utf-8 -*-
 # time: 2026/02/13
-# name: Haowen Cui, Yuhan Guo
+# name: Haowen Cui, Yuhan Guo, Selina Peng(sp4550)
 
 import matplotlib.pyplot as plt
 import pandas as pd
