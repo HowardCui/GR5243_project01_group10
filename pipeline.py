cebab15ceae42dd83fb2c5f54b3c18ef90698cc9
superwayne66 <166932130+superwayne66@users.noreply.github.com>
2026-02-20 04:36:18 -0500

    Update author names in pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python 3.12
 # -*- coding: utf-8 -*-
 # time: 2026/02/13
-# name: Haowen Cui, Yuhan Guo, Shiyue Peng sp4550,
+# name: Haowen Cui, Yuhan Guo, Shiyue Peng sp4550, Shengbo Yi
 
